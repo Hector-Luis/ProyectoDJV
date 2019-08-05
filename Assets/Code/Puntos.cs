3cd50044e0e96021b3719b8aa9f239259e5c8de2
dany94xd <danimaxi60@hotmail.com>
2019-08-05 08:00:51 -0500

    Marcador v.1

--- Assets/Code/Puntos.cs
+++ Assets/Code/Puntos.cs
@@ -6,19 +6,27 @@ public class Puntos : MonoBehaviour
 {
 
     public int puntos = 0;
+    public TextMesh marcador;
     // Start is called before the first frame update
     void Start()
     {
         NotificationCenter.DefaultCenter().AddObserver(this, "IncrementarPuntos");
+        ActualizarMarcador();
     }
 
     void IncrementarPuntos(Notification notification)
     {
         int puntosAIncrementar = (int)notification.data;
         puntos += puntosAIncrementar;
-        Debug.Log("Incrementado" + puntosAIncrementar + "Puntos. Total Ganados: " + puntos);
+        ActualizarMarcador();
+       Debug.Log("Incrementado" + puntosAIncrementar + "Puntos. Total Ganados: " + puntos);
     }
     // Update is called once per frame
+
+        void ActualizarMarcador()
+    {
+        marcador.text = puntos.ToString();
+    }
     void Update()
     {
         
