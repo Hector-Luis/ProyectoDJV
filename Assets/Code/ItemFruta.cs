c5a7ae624d930eace8908896453183a3ce9184ee
dany94xd <danimaxi60@hotmail.com>
2019-08-05 07:23:43 -0500

    item y suma de puntos

---
+++ Assets/Code/ItemFruta.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemFruta : MonoBehaviour
+{
+    public int puntosIncrementar = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            NotificationCenter.DefaultCenter().PostNotification(this, "IncrementarPuntos", puntosIncrementar);
+            //Debug.Log("tocado");
+          
+        }
+        Destroy(gameObject);//destruir el objeto
+    }
+}
